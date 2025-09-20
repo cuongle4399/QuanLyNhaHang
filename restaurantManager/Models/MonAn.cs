0c3e40cb12bd3ec8e46a3dd2544837038b7f2761
ThanhTin926 <thanhtinvonguyen926@gmail.com>
2025-09-20 11:12:04 +0700

    Test

--- restaurantManager/Models/MonAn.cs
+++ restaurantManager/Models/MonAn.cs
@@ -1,21 +1,127 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.RightsManagement;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel;
 
 namespace restaurantManager.Models
 {
-    public class MonAn
+    public class MonAn : INotifyPropertyChanged
     {
-        public int MaMonAn { get; set; }
-        public string TenMonAn { get; set; }
-        public float Gia { get; set; }
-        public string MoTa { get; set; }
-        public string HinhAnhURL { get; set; }
-        public int Loai { get; set; }
+        private int _maMonAn;
+        private string _tenMonAn;
+        private float _gia;
+        private string _moTa;
+        private string _hinhAnhURL;
+        private int _loai;
+        private int _soLuong;
+        private bool _isSelected;
 
+        public bool IsSelected
+        {
+            get => _isSelected;
+            set
+            {
+                if (_isSelected != value)
+                {
+                    _isSelected = value;
+                    OnPropertyChanged(nameof(IsSelected));
+                }
+            }
+        }
+
+        public int MaMonAn
+        {
+            get => _maMonAn;
+            set
+            {
+                if (_maMonAn != value)
+                {
+                    _maMonAn = value;
+                    OnPropertyChanged(nameof(MaMonAn));
+                }
+            }
+        }
+
+        public string TenMonAn
+        {
+            get => _tenMonAn;
+            set
+            {
+                if (_tenMonAn != value)
+                {
+                    _tenMonAn = value;
+                    OnPropertyChanged(nameof(TenMonAn));
+                }
+            }
+        }
+
+        public float Gia
+        {
+            get => _gia;
+            set
+            {
+                if (_gia != value)
+                {
+                    _gia = value;
+                    OnPropertyChanged(nameof(Gia));
+                    OnPropertyChanged(nameof(TongTien)); // cập nhật tổng tiền khi giá thay đổi
+                }
+            }
+        }
+
+        public string MoTa
+        {
+            get => _moTa;
+            set
+            {
+                if (_moTa != value)
+                {
+                    _moTa = value;
+                    OnPropertyChanged(nameof(MoTa));
+                }
+            }
+        }
+
+        public string HinhAnhURL
+        {
+            get => _hinhAnhURL;
+            set
+            {
+                if (_hinhAnhURL != value)
+                {
+                    _hinhAnhURL = value;
+                    OnPropertyChanged(nameof(HinhAnhURL));
+                }
+            }
+        }
+
+        public int Loai
+        {
+            get => _loai;
+            set
+            {
+                if (_loai != value)
+                {
+                    _loai = value;
+                    OnPropertyChanged(nameof(Loai));
+                }
+            }
+        }
+
+        public int SoLuong
+        {
+            get => _soLuong;
+            set
+            {
+                if (_soLuong != value)
+                {
+                    _soLuong = value;
+                    OnPropertyChanged(nameof(SoLuong));
+                    OnPropertyChanged(nameof(TongTien)); // cập nhật tổng tiền khi số lượng thay đổi
+                }
+            }
+        }
+
+        // Property tính tổng tiền cho từng món
+        public float TongTien => SoLuong * Gia;
 
         public MonAn(int maMonAn, string tenMonAn, float gia, string moTa, string hinhAnhURL, int loai)
         {
@@ -25,7 +131,20 @@ namespace restaurantManager.Models
             MoTa = moTa;
             HinhAnhURL = hinhAnhURL;
             Loai = loai;
+            SoLuong = 1;
         }
-    }
 
+        public MonAn()
+        {
+            SoLuong = 1;
+        }
+
+        // Triển khai INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
 }
