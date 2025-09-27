4af497c2c0bf7eb9c37403f98eb57ae75bd547cd
Cường Lê <161845711+cuongle4399@users.noreply.github.com>
2025-09-27 15:12:29 +0700

    update fuction staffManager

--- restaurantManager/obj/Debug/net8.0-windows/restaurantManager.AssemblyInfo.cs
+++ restaurantManager/obj/Debug/net8.0-windows/restaurantManager.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("restaurantManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d662fc8a452d958673721c58d34db90e1197d3d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3199050ceba849f27ed9f0a9d84200d45c061533")]
 [assembly: System.Reflection.AssemblyProductAttribute("restaurantManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("restaurantManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
